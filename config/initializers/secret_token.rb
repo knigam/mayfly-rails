caede3f5d855c491cef37213e89b2c3ab631a1ba
knigam <kushal.nigam1@gmail.com>
2014-01-21 16:42:22 -0500

    set up rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mayfly::Application.config.secret_key_base = '10eafdd981d7ba9bdf8cdd0eea7ccc4749d69ee351cc491dd44dabe63af62d94ac010a730f81942867696fb84e95d9246283232a6ed8ee9a774841dfccdea254'
