6d9e13dbb1084f7dbd401fa1a6d5c3c22488f783
Kushal Nigam <kushal@kush-server>
2014-02-24 14:57:56 -0500

    created constructors to override devise:sessions and registrations to disable csrf token on #create and allow :json

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Mayfly::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations", :sessions => "sessions"}
+  #devise_scope :user do
+  #    post 'users/sign_in' => "sessions#create"
+  #end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
